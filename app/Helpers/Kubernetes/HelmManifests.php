ed0b30e22aeab4d32d50aa099f6b6c6afcf959b9
snoweagle811 <snoweagle811@gmail.com>
2025-05-03 15:21:01 +0200

    feat: helm manifest generator

---
+++ app/Helpers/Kubernetes/HelmManifests.php
@@ -0,0 +1,328 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers\Kubernetes;
+
+use Exception;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use PharData;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class HelmManifests.
+ *
+ * This class is the helper for the helm manifests.
+ *
+ * @author Marcel Menk <marcel.menk@ipvx.io>
+ */
+class HelmManifests
+{
+    protected static string $repoUrl;
+
+    protected static string $chartName;
+
+    protected static string $repoName;
+
+    protected static string $namespace;
+
+    /**
+     * Generate the manifests for the helm chart.
+     *
+     * @param string $repoUrl
+     * @param string $chartName
+     * @param string $repoName
+     * @param string $namespace
+     *
+     * @return object
+     */
+    public static function generateManifests(string $repoUrl, string $chartName, string $repoName = 'my-helm-repo', string $namespace = 'default'): object
+    {
+        self::$repoUrl   = $repoUrl;
+        self::$chartName = $chartName;
+        self::$repoName  = $repoName;
+        self::$namespace = $namespace;
+
+        if (self::isOciRepo()) {
+            $chartPath = self::pullOciChart();
+        } else {
+            $index     = self::fetchIndexYaml();
+            $chartUrl  = self::getChartDownloadUrl($index);
+            $chartPath = self::downloadAndExtractChart($chartUrl);
+        }
+
+        $values = self::getChartValues($chartPath);
+
+        return (object) [
+            'helmrepository.yaml' => self::ensureYamlDocumentStart(self::generateHelmRepositoryYaml()),
+            'helmrelease.yaml'    => self::ensureYamlDocumentStart(self::generateHelmReleaseYaml($values)),
+            'kustomization.yaml'  => self::ensureYamlDocumentStart(self::generateKustomizationYaml()),
+        ];
+    }
+
+    /**
+     * Check if the repository is an OCI repository.
+     *
+     * @return bool
+     */
+    protected static function isOciRepo(): bool
+    {
+        return Str::startsWith(self::$repoUrl, 'oci://');
+    }
+
+    /**
+     * Fetch the index.yaml file from the repository.
+     *
+     * @return array
+     */
+    protected static function fetchIndexYaml(): array
+    {
+        $url      = rtrim(self::$repoUrl, '/') . '/index.yaml';
+        $response = Http::get($url);
+
+        if (!$response->successful()) {
+            throw new Exception("Failed to fetch index.yaml from: $url");
+        }
+
+        return Yaml::parse($response->body());
+    }
+
+    /**
+     * Get the chart download URL.
+     *
+     * @param array $index
+     *
+     * @return string
+     */
+    protected static function getChartDownloadUrl(array $index): string
+    {
+        if (!isset($index['entries'][self::$chartName])) {
+            $chartName = self::$chartName;
+            
+            throw new Exception("Chart '$chartName' not found in repository.");
+        }
+
+        $latest    = $index['entries'][self::$chartName][0];
+        $chartPath = $latest['urls'][0];
+
+        if (
+            Str::startsWith($chartPath, 'http') ||
+            Str::startsWith($chartPath, 'oci')
+        ) {
+            return $chartPath;
+        }
+
+        return rtrim(self::$repoUrl, '/') . '/' . ltrim($chartPath, '/');
+    }
+
+    /**
+     * Download and extract the chart.
+     *
+     * @param string $chartUrl
+     *
+     * @return string
+     */
+    protected static function downloadAndExtractChart(string $chartUrl): string
+    {
+        if (Str::startsWith($chartUrl, 'oci://')) {
+            return self::pullOciChartFromFullRef($chartUrl);
+        }
+
+        $filename = 'charts/' . Str::uuid();
+        $tgzPath  = Storage::path($filename . '.tgz');
+        Storage::put($filename . '.tgz', Http::get($chartUrl)->body());
+
+        $phar = new PharData($tgzPath);
+        $phar->decompress();
+        $tarPath = str_replace('.tgz', '.tar', $tgzPath);
+
+        $pharTar     = new PharData($tarPath);
+        $extractPath = Storage::path($filename);
+        $pharTar->extractTo($extractPath);
+
+        return $extractPath;
+    }
+
+    /**
+     * Pull the OCI chart.
+     *
+     * @return string
+     */
+    protected static function pullOciChart(): string
+    {
+        $tmpDir = storage_path('app/charts/' . Str::uuid());
+
+        if (!file_exists($tmpDir)) {
+            mkdir($tmpDir, 0755, true);
+        }
+
+        $ociRef = rtrim(self::$repoUrl, '/') . '/' . self::$chartName;
+
+        $cmd    = ['helm', 'pull', $ociRef, '--untar', '--destination', $tmpDir];
+        $result = Process::run($cmd);
+
+        if (!$result->successful()) {
+            throw new Exception('Failed to pull OCI chart: ' . $result->errorOutput());
+        }
+
+        return $tmpDir;
+    }
+
+    /**
+     * Pull the OCI chart from a full reference.
+     *
+     * @param string $ociRef
+     *
+     * @return string
+     */
+    protected static function pullOciChartFromFullRef(string $ociRef): string
+    {
+        $tmpDir = storage_path('app/charts/' . Str::uuid());
+
+        if (!file_exists($tmpDir)) {
+            mkdir($tmpDir, 0755, true);
+        }
+
+        $cmd    = ['helm', 'pull', $ociRef, '--untar', '--destination', $tmpDir];
+        $result = Process::run($cmd);
+
+        if (!$result->successful()) {
+            throw new Exception("Failed to pull OCI chart from full ref: $ociRef\n" . $result->errorOutput());
+        }
+
+        return $tmpDir;
+    }
+
+    /**
+     * Get the chart values.
+     *
+     * @param string $extractPath
+     *
+     * @return string
+     */
+    protected static function getChartValues(string $extractPath): string
+    {
+        $dirs = scandir($extractPath);
+
+        foreach ($dirs as $dir) {
+            if ($dir === '.' || $dir === '..') {
+                continue;
+            }
+            $valuesPath = "$extractPath/$dir/values.yaml";
+
+            if (file_exists($valuesPath)) {
+                return file_get_contents($valuesPath);
+            }
+        }
+
+        throw new Exception('values.yaml not found in chart directory.');
+    }
+
+    /**
+     * Generate the helm repository yaml.
+     *
+     * @return string
+     */
+    protected static function generateHelmRepositoryYaml(): string
+    {
+        if (self::isOciRepo()) {
+            return Yaml::dump([
+                'apiVersion' => 'source.toolkit.fluxcd.io/v1beta2',
+                'kind'       => 'HelmRepository',
+                'metadata'   => [
+                    'name'      => self::$repoName,
+                    'namespace' => self::$namespace,
+                ],
+                'spec' => [
+                    'type'     => 'oci',
+                    'url'      => self::$repoUrl,
+                    'interval' => '10m',
+                ],
+            ], 10, 2);
+        }
+
+        return Yaml::dump([
+            'apiVersion' => 'source.toolkit.fluxcd.io/v1beta2',
+            'kind'       => 'HelmRepository',
+            'metadata'   => [
+                'name'      => self::$repoName,
+                'namespace' => self::$namespace,
+            ],
+            'spec' => [
+                'url'      => self::$repoUrl,
+                'interval' => '10m',
+            ],
+        ], 10, 2);
+    }
+
+    /**
+     * Generate the helm release yaml.
+     *
+     * @param string $values
+     *
+     * @return string
+     */
+    protected static function generateHelmReleaseYaml(string $values): string
+    {
+        $base = [
+            'apiVersion' => 'helm.toolkit.fluxcd.io/v2beta1',
+            'kind'       => 'HelmRelease',
+            'metadata'   => [
+                'name'      => self::$chartName,
+                'namespace' => self::$namespace,
+            ],
+            'spec' => [
+                'interval'    => '5m',
+                'releaseName' => self::$chartName,
+                'chart'       => [
+                    'spec' => [
+                        'chart'     => self::$chartName,
+                        'sourceRef' => [
+                            'kind'      => 'HelmRepository',
+                            'name'      => self::$repoName,
+                            'namespace' => self::$namespace,
+                        ],
+                        'interval' => '1m',
+                    ],
+                ],
+                'values' => Yaml::parse($values),
+            ],
+        ];
+
+        return Yaml::dump($base, 10, 2);
+    }
+
+    /**
+     * Ensure the yaml document starts with a document start.
+     *
+     * @param string $yaml
+     *
+     * @return string
+     */
+    protected static function ensureYamlDocumentStart(string $yaml): string
+    {
+        $yaml = ltrim($yaml);
+
+        return Str::startsWith($yaml, '---') ? $yaml : "---\n" . $yaml;
+    }
+
+    /**
+     * Generate the kustomization yaml.
+     *
+     * @return string
+     */
+    protected static function generateKustomizationYaml(): string
+    {
+        return Yaml::dump([
+            'apiVersion' => 'kustomize.config.k8s.io/v1beta1',
+            'kind'       => 'Kustomization',
+            'resources'  => [
+                'helmrepository.yaml',
+                'helmrelease.yaml',
+            ],
+        ], 4, 2);
+    }
+}
