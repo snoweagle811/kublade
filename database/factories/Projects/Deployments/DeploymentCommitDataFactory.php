885aa81d4a978874e98c24fac9631d90fdf478ed
snoweagle811 <snoweagle811@gmail.com>
2025-05-29 21:24:29 +0200

    test: deployment commit data and commit secret data model

--- database/factories/Projects/Deployments/DeploymentCommitDataFactory.php
+++ database/factories/Projects/Deployments/DeploymentCommitDataFactory.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Database\Factories\Projects\Deployments;
 
+use App\Models\Projects\Deployments\Deployment;
 use App\Models\Projects\Deployments\DeploymentCommitData;
 use App\Models\Projects\Deployments\DeploymentData;
+use App\Models\Projects\Templates\TemplateField;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
+ * Class DeploymentCommitDataFactory.
+ *
+ * @author Marcel Menk <marcel.menk@ipvx.io>
+ *
  * @extends Factory<DeploymentCommitData>
  */
 class DeploymentCommitDataFactory extends Factory
@@ -27,11 +33,20 @@ class DeploymentCommitDataFactory extends Factory
      */
     public function definition(): array
     {
+        $deployment     = Deployment::factory()->create();
+        $templateField  = TemplateField::factory()->create();
+        $deploymentData = DeploymentData::factory()->create([
+            'deployment_id'     => $deployment->id,
+            'template_field_id' => $templateField->id,
+        ]);
+
         return [
-            'deployment_commit_id' => null,
-            'deployment_data_id'   => DeploymentData::factory(),
+            'deployment_commit_id' => null, // Should be set when creating
+            'deployment_data_id'   => $deploymentData->id,
             'key'                  => $this->faker->word,
-            'value'                => encrypt($this->faker->word),
+            'value'                => $this->faker->word,
+            'created_at'           => now(),
+            'updated_at'           => now(),
         ];
     }
 }
--- database/factories/Projects/Deployments/DeploymentCommitSecretDataFactory.php
+++ database/factories/Projects/Deployments/DeploymentCommitSecretDataFactory.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Database\Factories\Projects\Deployments;
 
+use App\Models\Projects\Deployments\Deployment;
 use App\Models\Projects\Deployments\DeploymentCommitSecretData;
 use App\Models\Projects\Deployments\DeploymentSecretData;
+use App\Models\Projects\Templates\TemplateField;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
+ * Class DeploymentCommitSecretDataFactory.
+ *
+ * @author Marcel Menk <marcel.menk@ipvx.io>
+ *
  * @extends Factory<DeploymentCommitSecretData>
  */
 class DeploymentCommitSecretDataFactory extends Factory
@@ -27,11 +33,20 @@ class DeploymentCommitSecretDataFactory extends Factory
      */
     public function definition(): array
     {
+        $deployment           = Deployment::factory()->create();
+        $templateField        = TemplateField::factory()->create();
+        $deploymentSecretData = DeploymentSecretData::factory()->create([
+            'deployment_id'     => $deployment->id,
+            'template_field_id' => $templateField->id,
+        ]);
+
         return [
-            'deployment_commit_id'      => null,
-            'deployment_secret_data_id' => DeploymentSecretData::factory(),
+            'deployment_commit_id'      => null, // Should be set when creating
+            'deployment_secret_data_id' => $deploymentSecretData->id,
             'key'                       => $this->faker->word,
-            'value'                     => encrypt($this->faker->word),
+            'value'                     => $this->faker->word,
+            'created_at'                => now(),
+            'updated_at'                => now(),
         ];
     }
 }
